fb0da94da1c8c5b436034df982a25db92ab41a94
Kristy L <kristyle96@yahoo.com>
2022-08-03 22:41:59 -0700

    Completed PyPoll Analysis

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,16 +16,38 @@ with open(output_path) as csvfile:
 
     # total amount of votes:
     t_votes = 0 #This will keep track of the vote counts
-    l_candidates = [] # Initialized the list to keep track of all the unique candidates that were voted
+    l_candidates = {} # Initialized the dictionary to keep track of all the unique candidates that were voted and their total votes
     for r in csvreader:
-        # print(1)
         t_votes += 1
-        if len(l_candidates) > 0:
-
-            for l_r in range(0, len(l_candidates)):
-                if r[2] != l_candidates[l_r]:
-                    l_candidates.append(r[2])
-        else: l_candidates.append(r[2])
+        if r[2] not in l_candidates:
+            l_candidates[r[2]] = 0
+        else:
+            l_candidates[r[2]] = l_candidates[r[2]] + 1
     
-print(t_votes)
-print(l_candidates)
+    # Calculate each candidate's percentage of votes
+    winner = "" # Initialize the winner variable which will hold the name of the winning candidate
+    for c in l_candidates.keys():
+        l_candidates[c] = [l_candidates[c], round(l_candidates[c]/t_votes*100, 3)]
+        # Determine who the winner is
+        if winner == "":
+            winner = c
+        else:
+            if l_candidates[c][0] > l_candidates[winner][0]:
+                winner = c
+        
+
+# Save the results as a string to display in the terminal and print to a text file
+t_txt = f"Total Votes: {t_votes}"
+w_txt = f"Winner: {winner}"
+c_txt = (f"Charles Casper Stockham: {l_candidates['Charles Casper Stockham'][1]}% ({l_candidates['Charles Casper Stockham'][0]})\n"
+f"Diana DeGette: {l_candidates['Diana DeGette'][1]}% ({l_candidates['Diana DeGette'][0]})\n"
+f"Raymon Anthony Doane: {l_candidates['Raymon Anthony Doane'][1]}% ({l_candidates['Raymon Anthony Doane'][0]})")
+
+display = ("Election Results\n----------------------\n"
+f"{t_txt}\n----------------------\n{c_txt}\n----------------------\n{w_txt}\n----------------------")
+
+print(display)
+with open('./Analysis/election_results.txt', 'w') as file:
+    writer = display
+    file.write(writer)
+    file.close()
